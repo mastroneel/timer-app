f34ec76318a35223a079d44e7ce6b7dff46dd973
Lauren Mastroni <laurenmastroni@gmail.com>
2017-05-03 12:30:58 +0800

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var Nav = require('Nav');
+
+var Main = (props) => {
+  return (
+    <div>
+      <div>
+        <Nav/>
+        <p>Main.jsx Rendered</p>
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
